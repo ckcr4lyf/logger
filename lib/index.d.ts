82b11c9d04109267c2f43237abce3cc63436857c
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-06 14:12:38 +0800

    fix types, only esm

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,17 @@
-export const LOGLEVEL = {
-    DEBUG: 'DEBUG',
-    INFO: 'INFO',
-    WARN: 'WARN',
-    ERROR: 'ERROR',
+type LOGLEVEL = 0 | 1 | 2 | 3;
+
+export const LOGLEVEL: Record<'DEBUG' | 'INFO' | 'WARN' | 'ERROR', LOGLEVEL> = {
+    'DEBUG': 0,
+    'INFO': 1,
+    'WARN': 2,
+    'ERROR': 3,
 }
 
 export class Logger {
-    constructor();
+
+    loglevel: LOGLEVEL;
+
+    constructor(loglevel: LOGLEVEL);
 
     debug(message: string): undefined;
 
