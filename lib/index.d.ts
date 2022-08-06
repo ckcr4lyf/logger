67be525727656ad3eefe87d947ecf9fb06be781c
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-06 14:22:00 +0800

    Fix types declaration

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,6 @@
 type LOGLEVEL = 0 | 1 | 2 | 3;
 
-export const LOGLEVEL: Record<'DEBUG' | 'INFO' | 'WARN' | 'ERROR', LOGLEVEL> = {
-    'DEBUG': 0,
-    'INFO': 1,
-    'WARN': 2,
-    'ERROR': 3,
-}
+export const LOGLEVEL: Record<'DEBUG' | 'INFO' | 'WARN' | 'ERROR', LOGLEVEL>;
 
 export class Logger {
 
